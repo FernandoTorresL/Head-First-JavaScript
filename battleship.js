5d039a245484a6d3ed3f1479c32c08518faafe70
Fernando Torres <fertorresmx@gmail.com>
2017-08-04 19:23:57 -0500

    Implementing the Model Object. Includes testing code. Chapter 8, pag.348

--- battleship.js
+++ battleship.js
@@ -13,11 +13,68 @@ var view = {
   }
 };
 
-view.displayMiss("00");
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+  ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] }, 
+          { locations: ["24", "34", "44"], hits: ["", "", ""] },
+          { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+/*view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
 view.displayHit("12");
 view.displayMiss("25");
 view.displayHit("26");
 
-view.displayMessage("Tap tap, is this thing on?");
+view.displayMessage("Tap tap, is this thing on?");*/
+
+
+/*Testing model object:*/
+model.fire("53"); // miss
+
+model.fire("06"); // hit
+model.fire("16"); // hit
+model.fire("26"); // hit
+
+model.fire("34"); // hit
+model.fire("24"); // hit
+model.fire("44"); // hit
+
+model.fire("12"); // hit
+model.fire("11"); // hit
+model.fire("10"); // hit
+
+
